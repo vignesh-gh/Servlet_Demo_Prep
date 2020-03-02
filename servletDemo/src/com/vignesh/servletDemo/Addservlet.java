65b4802a99fd77f51bc466124027c1663c962014
vigneshp <vigneshp@ELPBLR-049.evolvus.com>
2020-03-03 01:18:24 +0530

    Servlet-preparation

---
+++ servletDemo/src/com/vignesh/servletDemo/Addservlet.java
@@ -0,0 +1,30 @@
+package com.vignesh.servletDemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.oracle.jrockit.jfr.RequestDelegate;
+
+public class Addservlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException {
+		int i=Integer.parseInt(req.getParameter("num1"));
+		int j=Integer.parseInt(req.getParameter("num2"));
+		int k=i+j;
+	
+		req.setAttribute("k", k);
+		
+		RequestDispatcher rd= req.getRequestDispatcher("sq");
+		rd.forward(req, res);
+		
+
+	}
+
+	
+}
---
+++ servletDemo/src/com/vignesh/servletDemo/Sqservlet.java
@@ -0,0 +1,21 @@
+package com.vignesh.servletDemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Sqservlet extends HttpServlet{
+
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException {
+		
+		int k=(int) req.getAttribute("k");
+		k=k*k;
+		
+		PrintWriter pr= res.getWriter();
+		pr.println("square value = "+k);
+	}
+	
+}
